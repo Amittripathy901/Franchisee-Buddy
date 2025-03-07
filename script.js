eb6ac97f650c9def81693d4776c0543f80e2e040
Amittripathy901 <amittripathy.work@gmail.com>
2025-03-07 11:32:40 +0530

    Website files added

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('input[name="expert-option"]').forEach((radio) => {
+    radio.addEventListener("change", function () {
+        document.getElementById("email").disabled = false;
+        document.getElementById("phone").disabled = false;
+        document.getElementById("contact-btn").disabled = false;
+    });
+});
